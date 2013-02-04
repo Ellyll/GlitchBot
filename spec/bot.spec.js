71ca6523abaf04f0acd765d222cb7e1da25a5aa8
Ellyll <cawsllyffant-git@yahoo.com>
2013-02-04 18:04:36 +0000

    Added tests for commands.

--- spec/bot.spec.js
+++ spec/bot.spec.js
@@ -29,4 +29,158 @@ describe('command', function() {
             expect(botCommand).toBeNull();
         });
     });
+
+    describe('processCommand', function() {
+        var MockBot = function() {
+            this.sayCalled = false;
+            this.actionCalled = false;
+            this.disconnectCalled = false;
+
+            this.say = function() { this.sayCalled = true; }
+            this.action = function() { this.actionCalled = true; }
+            this.disconnect = function(message, shutdown) { this.disconnectCalled = true; shutdown(); }
+        }
+
+        var bot;
+        var botCommand;
+        var config;
+        var shutdown;
+        var shutdownCalled;
+
+        beforeEach(function(){
+            bot = new MockBot();
+            botCommand = { from: "someone", name: null, parameterText: null };
+            config = { channels: [ 'somechannel' ], botName: "someoneelse" };
+            shutdownCalled = false;
+            shutdown = function () { shutdownCalled = true; };
+        });
+
+        it('Should do nothing for an invalid command', function() {
+            botCommand.name = 'SOMEINVALIDCOMMAND';
+
+            command.processCommand(bot, botCommand, config, shutdown);
+            expect(bot.sayCalled).toBe(false);
+            expect(bot.actionCalled).toBe(false);
+            expect(bot.disconnectCalled).toBe(false);
+        });
+
+        it('Should for QUIT command, call disconnect() and shutdown() if from name the same as the bot\'s name', function(){
+            botCommand.name = 'QUIT';
+            botCommand.from = 'someone';
+            config.botName = 'someone';
+
+            command.processCommand(bot, botCommand, config, shutdown);
+            expect(bot.disconnectCalled).toBe(true);
+            expect(shutdownCalled).toBe(true);
+        });
+
+        it('Should for QUIT command, ignore it if from name is not the same as the bot\'s name', function(){
+            botCommand.name = 'QUIT';
+            botCommand.from = 'someone';
+            config.botName = 'someoneelse';
+
+            command.processCommand(bot, botCommand, config, shutdown);
+            expect(bot.disconnectCalled).toBe(false);
+            expect(shutdownCalled).toBe(false);
+        });
+
+        it('Should for SAY command call say() if from name the same as the bot\'s name', function() {
+            botCommand.name = 'SAY';
+            botCommand.from = 'someone';
+            config.botName = 'someone';
+
+            command.processCommand(bot, botCommand, config, shutdown);
+            expect(bot.sayCalled).toBe(true);
+            expect(bot.disconnectCalled).toBe(false);
+            expect(shutdownCalled).toBe(false);
+        });
+
+        it('Should for SAY command ignore it if from name is not the same as the bot\'s name', function() {
+            botCommand.name = 'SAY';
+            botCommand.from = 'someone';
+            config.botName = 'someoneelse';
+
+            command.processCommand(bot, botCommand, config, shutdown);
+            expect(bot.sayCalled).toBe(false);
+            expect(bot.disconnectCalled).toBe(false);
+            expect(shutdownCalled).toBe(false);
+        });
+
+        it('Should for EMOTE command call action() if from name the same as the bot\'s name', function() {
+            botCommand.name = 'EMOTE';
+            botCommand.from = 'someone';
+            config.botName = 'someone';
+
+            command.processCommand(bot, botCommand, config, shutdown);
+            expect(bot.actionCalled).toBe(true);
+            expect(bot.disconnectCalled).toBe(false);
+            expect(shutdownCalled).toBe(false);
+        });
+
+        it('Should for EMOTE command ignore it if from name is not the same as the bot\'s name', function() {
+            botCommand.name = 'EMOTE';
+            botCommand.from = 'someone';
+            config.botName = 'someoneelse';
+
+            command.processCommand(bot, botCommand, config, shutdown);
+            expect(bot.actionCalled).toBe(false);
+            expect(bot.disconnectCalled).toBe(false);
+            expect(shutdownCalled).toBe(false);
+        });
+
+        // say() based commands: 'DATE', 'DAY', 'MOONDAY'
+        it('Should call say() for the DATE command', function() {
+            botCommand.name = 'DATE';
+            command.processCommand(bot, botCommand, config, shutdown);
+            expect(bot.sayCalled).toBe(true);
+        });
+        it('Should call say() for the DAY command', function() {
+            botCommand.name = 'DAY';
+            command.processCommand(bot, botCommand, config, shutdown);
+            expect(bot.sayCalled).toBe(true);
+        });
+        it('Should call say() for the MOONDAY command', function() {
+            botCommand.name = 'MOONDAY';
+            command.processCommand(bot, botCommand, config, shutdown);
+            expect(bot.sayCalled).toBe(true);
+        });
+
+        // action() based commands: 'MOON', 'DANCE', 'HUG', 'KISS', 'SPLANK', 'POKE', 'HI'
+        it('Should call action() for the MOON command', function() {
+            botCommand.name = 'MOON';
+            command.processCommand(bot, botCommand, config, shutdown);
+            expect(bot.actionCalled).toBe(true);
+        });
+        it('Should call action() for the DANCE command', function() {
+            botCommand.name = 'DANCE';
+            command.processCommand(bot, botCommand, config, shutdown);
+            expect(bot.actionCalled).toBe(true);
+        });
+        it('Should call action() for the HUG command', function() {
+            botCommand.name = 'HUG';
+            command.processCommand(bot, botCommand, config, shutdown);
+            expect(bot.actionCalled).toBe(true);
+        });
+        it('Should call action() for the KISS command', function() {
+            botCommand.name = 'KISS';
+            command.processCommand(bot, botCommand, config, shutdown);
+            expect(bot.actionCalled).toBe(true);
+        });
+        it('Should call action() for the SPLANK command', function() {
+            botCommand.name = 'SPLANK';
+            command.processCommand(bot, botCommand, config, shutdown);
+            expect(bot.actionCalled).toBe(true);
+        });
+        it('Should call action() for the POKE command', function() {
+            botCommand.name = 'POKE';
+            command.processCommand(bot, botCommand, config, shutdown);
+            expect(bot.actionCalled).toBe(true);
+        });
+        it('Should call action() for the HI command', function() {
+            botCommand.name = 'HI';
+            command.processCommand(bot, botCommand, config, shutdown);
+            expect(bot.actionCalled).toBe(true);
+        });
+
+    });
 });
