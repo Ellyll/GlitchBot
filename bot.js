906b8e6c8e7b4d7f04c0f0bbcb8dc564bb1ae316
Ellyll <cawsllyffant-git@yahoo.com>
2013-02-10 11:40:02 +0000

    Created namespace for global variables.

--- bot.js
+++ bot.js
@@ -3,36 +3,79 @@ var config = require("./config");
 var irc = require("irc");
 var glitch = require('./lib/Glitch/glitch'); // the IDE changed this for me when I moved the files :)
 var command = require('./lib/Bot/command');
-var lastGlitchDate = new glitch.GlitchDate();
-var announcementsEnabled = false;
+//var lastGlitchDate = new glitch.GlitchDate();
+//var announcementsEnabled = false;
+var glitchBot = { // namespace for app's globals
+    bot : null,
+    announcementsEnabled : false,
+    lastGlitchDate : new glitch.GlitchDate()
+};
+
+//-----------
+// Functions
+//-----------
+function shutdown() {
+    console.log('Shutting down');
+    process.exit();
+}
+
+function announceTime() {
+    var newGlitchDate = new glitch.GlitchDate();
+    if (newGlitchDate.dayOfYear !== glitchBot.lastGlitchDate.dayOfYear) {
+        if (glitchBot.announcementsEnabled) {
+            glitchBot.bot.say(config.channels[0], 'Happy New ' + newGlitchDate.dayOfWeek.name + '!');
+            glitchBot.bot.say(config.channels[0], 'The time is ' + newGlitchDate.toString());
+            if (newGlitchDate.dayOfWeek.name === 'Moonday') {
+                glitchBot.bot.action(config.channels[0], 'moons everyone!');
+            }
+        }
+    }
+    glitchBot.lastGlitchDate = newGlitchDate;
+    setTimeout(announceTime, 2000);
+}
+
+function processMessage(from, to, text, message) {
+    var botCommand = command.getCommand(from, text);
+    if ( botCommand === null ) {
+        return;
+    }
+    command.processCommand(glitchBot.bot, botCommand, config, shutdown);
+}
+
+
+
+//------
+// Main
+//------
+
 
 // Create the bot name
-var bot = new irc.Client(config.server, config.botName, {
+glitchBot.bot = new irc.Client(config.server, config.botName, {
   channels: config.channels
 });
 
 // Listen for errors
-bot.addListener("error", function(message) {
+glitchBot.bot.addListener("error", function(message) {
   console.log(message);
 });
 
 // Listen for any message
-bot.addListener("message", processMessage);
+glitchBot.bot.addListener("message", processMessage);
 
 // Enable time announcements only when we're in the channel
 // also give op to LazyCubimal since bot often beats her on after server restart
-bot.addListener("join#moonies", function (nick) {
+glitchBot.bot.addListener("join#moonies", function (nick) {
   if ( nick === config.botName ) {
-    announcementsEnabled = true;
+    glitchBot.announcementsEnabled = true;
   }
   if ( nick === 'LazyCubimal' ) {
     console.log('Giving op to LazyCubimal');
-    bot.send('MODE', '#moonies', '+o', 'LazyCubimal');
+    glitchBot.bot.send('MODE', '#moonies', '+o', 'LazyCubimal');
   }
 });
-bot.addListener("part#moonies", function(nick) {
+glitchBot.bot.addListener("part#moonies", function(nick) {
   if ( nick === config.botName ) {
-    announcementsEnabled = false;
+    glitchBot.announcementsEnabled = false;
   }
 });
 
@@ -48,34 +91,4 @@ process.stdin.on('data', function (text) {
 });
 
 
-//-----------
-// Functions
-//-----------
 
-function announceTime() {
-    var newGlitchDate = new glitch.GlitchDate();
-    if (newGlitchDate.dayOfYear !== lastGlitchDate.dayOfYear) {
-        if (announcementsEnabled) {
-            bot.say(config.channels[0], 'Happy New ' + newGlitchDate.dayOfWeek.name + '!');
-            bot.say(config.channels[0], 'The time is ' + newGlitchDate.toString());
-            if (newGlitchDate.dayOfWeek.name == 'Moonday') {
-                bot.action(config.channels[0], 'moons everyone!');
-            }
-        }
-    }
-    lastGlitchDate = newGlitchDate;
-    setTimeout(announceTime, 2000);
-}
-
-function processMessage(from, to, text, message) {
-    var botCommand = command.getCommand(from, text);
-    if ( botCommand === null ) {
-        return;
-    }
-    command.processCommand(bot, botCommand, config, shutdown);
-}
-
-function shutdown() {
-    console.log('Shuting down');
-    process.exit();
-}
